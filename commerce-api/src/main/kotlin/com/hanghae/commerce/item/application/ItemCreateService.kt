52fc22884a2a402622f46a40a0ddee81dc9d09b4
e-build <donggeol2@daum.net>
2024-01-06 23:41:44 +0900

    docs: 도메인 모델 <-> 엔티티 모델 간 변환 전략 고민 정리

--- commerce-api/src/main/kotlin/com/hanghae/commerce/item/application/ItemCreateService.kt
+++ commerce-api/src/main/kotlin/com/hanghae/commerce/item/application/ItemCreateService.kt
@@ -4,13 +4,16 @@ import com.hanghae.commerce.item.domain.Item
 import com.hanghae.commerce.item.infrastructure.ItemWriter
 import com.hanghae.commerce.item.api.dto.CreateItemRequest
 import com.hanghae.commerce.item.api.dto.CreateItemResponse
+import com.hanghae.commerce.item.infrastructure.ItemReader
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Propagation
 import org.springframework.transaction.annotation.Transactional
 import java.util.*
 
 @Service
 class ItemCreateService(
     private val itemWriter: ItemWriter,
+    private val itemReader: ItemReader
 ) {
 
     @Transactional
@@ -24,7 +27,16 @@ class ItemCreateService(
         )
 
         val savedItem = itemWriter.save(item)
+        modifyItem(savedItem.id)
 
         return CreateItemResponse.of(savedItem)
+    }
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    fun modifyItem(itemId: String) {
+        val item = itemReader.getItemByItemId(itemId)
+        item?.let {
+            it.name = "${item.name}_modified"
+        }
     }
 }
--- commerce-api/src/main/kotlin/com/hanghae/commerce/store/domain/StoreReadService.kt
+++ commerce-api/src/main/kotlin/com/hanghae/commerce/store/domain/StoreReadService.kt
@@ -1,16 +1,13 @@
 package com.hanghae.commerce.store.domain
 
-import com.hanghae.commerce.common.async.AsyncLogPrinter
 import com.hanghae.commerce.store.infrastructure.StoreReader
 import org.springframework.stereotype.Service
 
 @Service
 class StoreReadService(
     private val storeReader: StoreReader,
-    private val asyncLogPrinter: AsyncLogPrinter,
 ) {
     fun read(): List<Store> {
-        asyncLogPrinter.print()
         return storeReader.findAll()
     }
 
